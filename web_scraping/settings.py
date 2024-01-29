807681a51ae166982e4ad2f8d762a5205cf9ee6a
Abdul Wassay Ansari <75994908+abdulwassay786@users.noreply.github.com>
2024-01-29 17:11:59 +0500

    Update settings.py

--- web_scraping/settings.py
+++ web_scraping/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # STATICFILES_DIRS = [str(PROJECT_PACKAGE.joinpath("static"))]
-# STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
